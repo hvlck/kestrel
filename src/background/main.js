c3994ecb07d62ef24181fb991307a70ff716d01e
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-05-07 16:45:10 -0400

    feat: added options page
    + Added options page

--- src/background/main.js
+++ src/background/main.js
@@ -1,9 +1,18 @@
+browser.runtime.onInstalled.addListener(() => {
+    browser.runtime.openOptionsPage().catch(err => {
+
+    });
+});
+
+// Listens for commands
 browser.commands.onCommand.addListener(command => {
-    if (command === 'activate') {
-        browser.tabs.executeScript({
+    console.log(command) // Remove
+    if (command === 'activate') { // Open UI command
+        browser.tabs.executeScript({ // Injects main UI script
             file: '../contentscripts/ui.js'
         });
-        browser.tabs.insertCSS({
+
+        browser.tabs.insertCSS({ // Injects UI stylesheet
             cssOrigin: "author",
             file: "../contentscripts/ui.css"
         })
---
+++ src/libs/commandpal.js
@@ -0,0 +1,1 @@
+// Future home of the command processor
