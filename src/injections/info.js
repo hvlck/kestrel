bad6223cb6caa3cb992f6ecff82023ffddda250a
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-08-18 18:47:55 -0400

    feat: add rss notification

--- src/injections/info.js
+++ src/injections/info.js
@@ -1,5 +1,29 @@
-browser.runtime.onMessage.addListener((msg, sender) => {
-    if (msg.start === true) {
-        browser.runtime.sendMessage({ info: `init` });
-    }
-});
+(function () {
+    const data = {
+        getRSS: () => {
+            let links = [];
+            document.head
+                .querySelectorAll(
+                    'link[type="application/atom+xml"],link[type="application/feed+json"],link[type="rss+xml"]'
+                )
+                .forEach((item) => {
+                    links.push(item.href);
+                });
+
+            return links;
+        },
+    };
+
+    const scrape = () => {
+        let returnedData = {};
+        returnedData.rss = data.getRSS();
+
+        return returnedData;
+    };
+
+    browser.runtime.onMessage.addListener((msg, sender) => {
+        if (msg.start === true) {
+            browser.runtime.sendMessage({ info: scrape() });
+        }
+    });
+})();
--- src/page_action/index.js
+++ src/page_action/index.js
@@ -2,11 +2,59 @@ import { getActiveTab, execute, activeTab } from '../libs/webUtils.js';
 import buildElement from '../libs/utils.js';
 
 const dispatch = (event, status) => {
-    document.body.appendChild(
+    main.appendChild(
         buildElement('p', event, {
             className: `banner ${status}`,
         })
     );
+};
+
+const main = buildElement('div', '', {
+    id: 'main',
+});
+
+document.body.appendChild(main);
+
+const home = buildElement('a', 'Back', {
+    style: 'display:none',
+});
+
+home.addEventListener('click', () => menus.showMain());
+
+const menus = {
+    showMain: () => {
+        document.body.appendChild(main);
+    },
+
+    showHome: () => {
+        home.style.display = '';
+    },
+
+    showFeeds: (feeds) => {
+        menus.showHome();
+        main.remove();
+        feeds.forEach((item) => {
+            document.body.appendChild(
+                buildElement('a', item, {
+                    href: item,
+                })
+            );
+        });
+    },
+};
+
+let info = {};
+
+const buildFromInfo = () => {
+    console.log(info);
+    if (info.rss.length != 0) {
+        let rss = buildElement('a', 'RSS Feeds Available');
+        rss.addEventListener('click', () => {
+            menus.showFeeds(info.rss);
+        });
+
+        main.appendChild(rss);
+    }
 };
 
 getActiveTab().then(async (t) => {
@@ -25,20 +73,24 @@ getActiveTab().then(async (t) => {
                 dispatch('Failed to copy!', 'f');
             });
     });
-    document.body.appendChild(c);
+    main.appendChild(c);
 
-    console.warn(t);
-    let siteDataLoader = buildElement('p', 'Loading');
+    let siteDataLoader = buildElement('p', 'Loading...');
 
     let registered = execute('../injections/info.js')
         .then(async () => {
             let t = await activeTab();
+            main.appendChild(siteDataLoader);
             browser.runtime.onMessage.addListener((msg, sender) => {
                 if (sender.id !== browser.runtime.id) return;
+                if (msg.info && Object.keys(info).length == 0) {
+                    siteDataLoader.remove();
+                    info = msg.info;
+                    buildFromInfo();
+                }
             });
-            browser.tabs.sendMessage(t[0].id, { start: true }).then(() => {
-                document.body.appendChild(siteDataLoader);
-            });
+
+            browser.tabs.sendMessage(t[0].id, { start: true }).then(() => {});
         })
         .catch((err) => {
             console.error(`Failed to inject info: ${err}`);
