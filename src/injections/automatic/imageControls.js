87fcef31f41fedda1a67745e22add3b94a7bab3a
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-09-18 19:45:55 -0400

    feat: parse original image into canvas

--- src/injections/automatic/imageControls.js
+++ src/injections/automatic/imageControls.js
@@ -1,1 +1,39 @@
-(function () {})();
+(function () {
+    const buildElement = (type, text, attributes) => {
+        let element = document.createElement(type);
+        element.innerText = text || '';
+        if (attributes) {
+            Object.keys(attributes).forEach((item) => {
+                if (item.includes('data_')) {
+                    element.setAttribute(item.replace(new RegExp('_', 'g'), '-'), attributes[item]);
+                } else {
+                    element[item] = attributes[item];
+                }
+            });
+        }
+        return element;
+    };
+
+    const container = buildElement('div', '', { style: 'margin:auto;text-align:center' });
+
+    const img = new Image();
+    img.src = document.body.querySelector('img').src;
+
+    const canvas = buildElement('canvas', '', {
+        style: `max-width:80%;margin:auto;text-align:center;`,
+    });
+    const ctx = canvas.getContext('2d');
+    container.appendChild(canvas);
+
+    img.addEventListener('load', function () {
+        canvas.height = this.naturalHeight;
+        canvas.width = this.naturalWidth;
+
+        ctx.drawImage(this, 0, 0);
+    });
+
+    document.body.querySelector('img').remove();
+    document.body.appendChild(container);
+
+    const toolbar = buildElement('div');
+})();
